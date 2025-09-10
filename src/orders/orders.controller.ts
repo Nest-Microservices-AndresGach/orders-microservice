e0e7349e418eabd57b32af84733dd1786912afc8
Andres <andresf.gacharnat@utadeo.edu.co>
2025-09-10 14:05:57 -0500

    feat: Payments incorporated for sales

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -4,7 +4,12 @@ import {
   NotImplementedException,
   ParseUUIDPipe,
 } from '@nestjs/common';
-import { ClientProxy, MessagePattern, Payload } from '@nestjs/microservices';
+import {
+  ClientProxy,
+  EventPattern,
+  MessagePattern,
+  Payload,
+} from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { OrderPaginationDto } from './dto/order-pagination.dto';
@@ -15,9 +20,16 @@ import { PRODUCT_SERVICE } from 'src/config';
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  // * ABAJO PODEMOS VER QUE EN VEZ DE MESSAGEPATTERN (M.P.) HAY UN EVENTPATTERN, LA DIFERENCIA ES QUE EL E.P. SOLO ESCUCHA, MIENTRAS QUE M.P. ENVÍA Y RECIBE INFORMACIÓN
   @MessagePattern({ cmd: 'createOrder' })
-  create(@Payload() createOrderDto: CreateOrderDto) {
-    return this.ordersService.create(createOrderDto);
+  async create(@Payload() createOrderDto: CreateOrderDto) {
+    const order = await this.ordersService.create(createOrderDto);
+    const paymentSession = await this.ordersService.createPaymentSession(order);
+
+    return {
+      order,
+      paymentSession,
+    };
   }
 
   @MessagePattern({ cmd: 'findAllOrders' })
@@ -33,5 +45,12 @@ export class OrdersController {
   @MessagePattern({ cmd: 'changeOrderStatus' })
   changeOrderStatus(@Payload() changeOrderStatusDto: changeOrderStatusDto) {
     return this.ordersService.changeStatus(changeOrderStatusDto);
+  }
+
+  // @MessagePattern() // * De hecho se pueden colocar ambos al tiempo en caso de que se requiera pero en este ejemplo no. Solamente lo puse para acordarme.
+  @EventPattern('payment.succeeded')
+  paidOrder(@Payload() paidOrderDto: any) {
+    console.log({ paidOrderDto });
+    return this.ordersService.paidOrder(paidOrderDto);
   }
 }
